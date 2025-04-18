dd942a5ad01bce9b75c20aaf02136a2c386ca702
P MANOHARA REDDY <manoharareddyp97@gmail.com>
2025-04-18 18:23:25 +0530

    Data Fetching part is updated

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import matplotlib.pyplot as plt
 import seaborn as sns
-from flask import Flask, request, render_template, url_for, session, redirect, flash
+from flask import Flask, request, render_template, url_for, session, redirect, flash, jsonify
 import praw
 import pandas as pd
 import requests
@@ -23,9 +23,31 @@ news_api_key = 'f1c1b88c5f55436596e8df23d2a7649b'
 # Event Registry Credentials
 event_registry_api_key = '354393d0-e38d-4735-9b99-fd99a8edc17f'
 
-
 analyzer = SentimentIntensityAnalyzer()
 
+def reddit_analyis(key):
+    posts = []
+    for post in reddit.subreddit("technology").search(key,limit = 100):
+        posts.append({
+            "id":post.id,
+            "title":post.title,
+            "selftext" : post.selftext,
+            "created utc":post.created_utc,
+            "score":post.score,
+        })
+    return posts
+
+def news_Analysis(key):
+    url = f'https://newsapi.org/v2/everything?q={key}&apiKey={news_api_key}&pageSize=100'
+    response = requests.get(url)
+    articles = response.json().get('articles', [])
+    return articles
+
+def EventRegistry_analysis(key):
+    url = f'https://eventregistry.org/api/v1/article/getArticles?query=%7B%22%24query%22%3A%7B%22keyword%22%3A%22{key}%22%2C%22lang%22%3A%22eng%22%7D%2C%22%24filter%22%3A%7B%22forceMaxDataTimeWindow%22%3A%2231%22%7D%7D&resultType=articles&articlesSortBy=date&articlesPage=1&articlesCount=50&apiKey={event_registry_api_key}'
+    response = requests.get(url)
+    articles = response.json().get('articles', [])
+    return articles
 
 @app.route('/')
 def home():
@@ -34,14 +56,11 @@ def home():
 @app.route('/dashboard', methods = ["POST"])
 def dashboard():
     key = request.form.get('keyword')
-    """
-    url = f'https://newsapi.org/v2/everything?q={key}&apiKey={news_api_key}&pageSize=100'
-    """
-    
-    url = f'https://eventregistry.org/api/v1/article/getArticles?query=%7B%22%24query%22%3A%7B%22keyword%22%3A%22{key}%22%2C%22lang%22%3A%22eng%22%7D%2C%22%24filter%22%3A%7B%22forceMaxDataTimeWindow%22%3A%2231%22%7D%7D&resultType=articles&articlesSortBy=date&articlesPage=1&articlesCount=50&apiKey={event_registry_api_key}'    
-    response = requests.get(url)
-    articles = response.json().get('articles', [])
-    return articles
+    reddit_posts = reddit_analyis(key)
+    EventRegistry_posts = EventRegistry_analysis(key)
+    news_articles = news_Analysis(key)
+
+    return jsonify({"reddit posts":reddit_posts , "Event Registry Articles":EventRegistry_posts, "News Articles":news_articles})
 
 if __name__ == "__main__":
     app.run(debug = True)
